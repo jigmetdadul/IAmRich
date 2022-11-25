b4e6235c5a3f41ef835852c7034070b6dffc9889
Jigmet Stanzin Dadul <jigmetdadul6@gmail.com>
2022-11-25 22:15:20 +0530

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Jigmet Stanzin Dadul on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
